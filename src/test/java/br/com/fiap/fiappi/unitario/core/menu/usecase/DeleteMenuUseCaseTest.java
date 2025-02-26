f007837ed0307f2d4416100194b797a631cffd8f
João Victor <joaovictorf01@gmail.com>
2025-02-25 23:14:54 -0300

    adicionando  testes para  menu

--- src/test/java/br/com/fiap/fiappi/unitario/core/menu/usecase/DeleteMenuUseCaseTest.java
+++ src/test/java/br/com/fiap/fiappi/unitario/core/menu/usecase/DeleteMenuUseCaseTest.java
@@ -1,20 +1,21 @@
 package br.com.fiap.fiappi.unitario.core.menu.usecase;
 
-import br.com.fiap.fiappi.core.menu.dto.MenuDTO;
-import br.com.fiap.fiappi.core.menu.gateway.ImageGateway;
-import br.com.fiap.fiappi.core.menu.gateway.MenuGateway;
-import br.com.fiap.fiappi.core.menu.usecase.DeleteMenuUseCaseImpl;
-import com.google.gson.Gson;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.UUID;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import br.com.fiap.fiappi.core.menu.gateway.ImageGateway;
+import br.com.fiap.fiappi.core.menu.gateway.MenuGateway;
+import br.com.fiap.fiappi.core.menu.usecase.DeleteMenuUseCaseImpl;
 
 public class DeleteMenuUseCaseTest {
 
